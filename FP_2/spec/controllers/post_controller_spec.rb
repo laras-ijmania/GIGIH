d86c7b271941cb399c8537c6a45a51355887aac5
laras-ijmania <laras.ijmania@gmail.com>
2021-08-21 02:53:19 +0700

    test if the input when create post is invalid

--- FP_2/spec/controllers/post_controller_spec.rb
+++ FP_2/spec/controllers/post_controller_spec.rb
@@ -30,4 +30,58 @@ describe '#create' do
       expect(result).not_to be_nil
     end
   end
+
+  context 'given valid parameter' do
+    it 'make a new comment' do
+      stub = double
+  
+      controller = PostController.new
+      params = {
+        "id": 1,
+        "content": 'a',
+        "attachment": '',
+        "user_name": 'b'
+      }
+      post = Post.new(params)
+  
+      expect(Post).to receive(:new).with(params).and_return(stub)
+      expect(stub).to receive(:valid?).and_return(true)
+      expect(User).to receive(:user_by_name?).and_return(true)
+      expect(stub).to receive(:save).and_return(true)
+      expect(Post).to receive(:find_by_id).with(1).and_return(post)
+      expect(stub).to receive(:comment?).and_return(true)
+      expect(stub).to receive(:relatedId).and_return(1)
+      expect(Post).to receive(:post_by_id?).with(1).and_return(true)
+  
+      result_string = controller.create(params)
+      expect(result_string).to eq('post created')
+  
+      result = Post.find_by_id(1)
+      expect(result).not_to be_nil
+    end
+  end
+  
+  context 'given related id not found' do
+    it 'return post not found' do
+      stub = double
+  
+      controller = PostController.new
+      params = {
+        "id": 1,
+        "content": 'a',
+        "attachment": '',
+        "user_name": 'b'
+      }
+  
+      expect(Post).to receive(:new).with(params).and_return(stub)
+      expect(stub).to receive(:valid?).and_return(true)
+      expect(User).to receive(:user_by_name?).and_return(true)
+      expect(stub).to receive(:comment?).and_return(true)
+      expect(stub).to receive(:relatedId).and_return(1)
+      expect(Post).to receive(:post_by_id?).with(1).and_return(false)
+  
+      result_string = controller.create(params)
+      expect(result_string).to eq('post not found')
+    end
+  end
 end
