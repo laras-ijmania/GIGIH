27ff0f752836b76d9332d8c487606ff07dd53d7c
laras-ijmania <laras.ijmania@gmail.com>
2021-08-21 03:26:07 +0700

    renaming endpoints

--- FP_2/app.rb
+++ FP_2/app.rb
@@ -2,3 +2,60 @@ require 'sinatra'
 require 'sinatra/namespace'
 require './controllers/user_controller'
 require './controllers/post_controller'
+
+get '/' do
+    return 'Hello world'
+  end
+  
+  before do
+    content_type 'application/json'
+  end
+  
+  namespace '/users' do
+    controller = UserController.new
+  
+    get '' do
+      controller.index
+    end
+  
+    get '/:name' do
+      controller.show(params)
+    end
+  
+    post '' do
+      controller.create(params)
+    end
+  end
+  
+  namespace '/posts' do
+    controller = PostController.new
+  
+    get '' do
+      controller.index
+    end
+  
+    get '/:id' do
+      controller.show(params)
+    end
+  
+    post '' do
+      controller.create(params)
+    end
+  end
+  
+  namespace '/trending' do
+    controller = PostController.new
+  
+    get '' do
+      controller.trending
+    end
+  end
+  
+  namespace '/search' do
+    controller = PostController.new
+  
+    get '/:str' do
+      controller.search(params)
+    end
+  end
+  
